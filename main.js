42ea9550d30264a49cc7044a404781a62c590b35
yusupovsaidazim810 <yusupovsaidazim810@gmail.com>
2026-03-13 10:41:52 +0500

    full

---
+++ main.js
@@ -0,0 +1,72 @@
+// window.addEventListener("load", function () {
+//     setTimeout(function () {
+//         document.getElementById("loader").style.display = "none";
+//     }, 2000);
+// });
+
+function darkMode() {
+    document.body.classList.toggle("dark");
+}
+
+
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+for (let i = 0; i < 100; i++) {
+    particles.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        vx: (Math.random() - 0.5) * 1,
+        vy: (Math.random() - 0.5) * 1,
+        size: Math.random() * 3
+    });
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach(p => {
+        p.x += p.vx;
+        p.y += p.vy;
+
+        if (p.x < 0 || p.x > canvas.width) p.vx *= -1;
+        if (p.y < 0 || p.y > canvas.height) p.vy *= -1;
+
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
+        ctx.fillStyle = "white";
+        ctx.fill();
+    });
+
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+
+document.addEventListener("mousemove", function (e) {
+
+    let dot = document.createElement("div");
+    dot.className = "cursor-dot";
+
+    dot.style.left = e.pageX + "px";
+    dot.style.top = e.pageY + "px";
+
+    document.body.appendChild(dot);
+
+    setTimeout(() => {
+        dot.remove();
+    }, 500);
+
+});
+
+
+
+
+
+
