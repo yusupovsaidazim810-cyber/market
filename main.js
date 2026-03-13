98c8168a2cdd24e6e43f5f4edcfcf161727b229b
yusupovsaidazim810 <yusupovsaidazim810@gmail.com>
2026-03-13 14:42:11 +0500

    market

--- main.js
+++ main.js
@@ -67,6 +67,50 @@ document.addEventListener("mousemove", function (e) {
 
 
 
+// const canvas = document.getElementById("stars");
+// const ctx = canvas.getContext("2d");
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+// let stars = [];
+
+// for (let i = 0; i < 150; i++) {
+//     stars.push({
+//         x: Math.random() * canvas.width,
+//         y: Math.random() * canvas.height,
+//         size: Math.random() * 2,
+//         speed: Math.random() * 1
+//     });
+// }
+
+// function animate() {
+
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     ctx.fillStyle = "white";
+
+//     stars.forEach(star => {
+
+//         star.y += star.speed;
+
+//         if (star.y > canvas.height) {
+//             star.y = 0;
+//         }
+
+//         ctx.beginPath();
+//         ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
+//         ctx.fill();
+
+//     });
+
+//     requestAnimationFrame(animate);
+// }
+
+// animate();
+
+
+
 
 
 
